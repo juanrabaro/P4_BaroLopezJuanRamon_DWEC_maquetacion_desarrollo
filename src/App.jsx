0ac311124c3baf64cab01861b166c44c57767b65
juanrabaro <jrbaro@iesrafaelalberti.es>
2023-12-07 18:41:56 +0100

    maquetacion avanzada

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,27 @@ import Pag404 from './components/Pag404'
 
 // Con <Link to="/">Home</Link> importado desde "react-router-dom"
 // haria que sea SPA(single page application) porque no recargaría la página
+// /:name
+// useParams
+// <Outlet />
 
 function App() {
   return (
     <>
       <header>
-        <h1>I am the header</h1>
+        <img src='/src/assets/images/logo.png' alt="logo" />
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/contacto">Contacto</a></li>
+          <li><a href="/wiki">Wiki</a></li>
+          <li><a href="/iniciarSesion">Iniciar sesión</a></li>
+          <li><a href="/crearCuenta">Crear cuenta</a></li>
+        </ul>
       </header>
-      <main>
-        <Routes>
-          <Route path='/' element={ <HomePag /> } />
-          <Route path='' errorElement={ <Pag404 /> } />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path='/' element={ <HomePag /> } />
+        <Route path='/contacto' element={ <Pag404 /> } />
+      </Routes>
       <footer>
         <h1>I am the footer</h1>
       </footer>
--- src/components/HomePag.jsx
+++ src/components/HomePag.jsx
@@ -2,7 +2,47 @@ import React from "react"
 
 function HomePag() {
   return (
-      <h1>HOMEPAG</h1>
+    <main className="main-home">
+      <section className="main-home__primer-section-home">
+        <a href="/" className="tituloHome">GENERAR CAT FACT</a>
+        <img className="imgPrincipal" src="/src/assets/images/imagenPrincipal.png" alt="imagenPrincipal" />
+      </section>
+      <section className="main-home__segundo-section-home">
+        <form className="segundo-section-home__form-iniciar-sesion">
+          <fieldset className="form-iniciar-sesion__fieldset">
+            <h2>Inicio sesión</h2>
+            <div>
+              <p>
+                <label>Gmail:</label>
+                <input type="text"></input>
+              </p>
+              <p>
+                <label>Contraseña:</label>
+                <input type="text"></input>
+              </p>
+            </div>
+            <a href="/">Iniciar Sesión</a>
+          </fieldset>
+        </form>
+
+        <form className="segundo-section-home__form-crear-cuenta">
+          <fieldset className="form-crear-cuenta__fieldset">
+            <h2>Crear cuenta</h2>
+            <div>
+              <p>
+                <label>Gmail:</label>
+                <input type="text"></input>
+              </p>
+              <p>
+                <label>Contraseña:</label>
+                <input type="text"></input>
+              </p>
+            </div>
+            <a href="/">Crear Cuenta</a>
+          </fieldset>
+        </form>
+      </section>
+    </main>
   )
 }
 
