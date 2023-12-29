73468971f2119b9cabf0b23b38cfa70d0812e393
juanrabaro <jrbaro@iesrafaelalberti.es>
2023-12-29 21:50:55 +0100

    get random fact hecho

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LoginForm from '../components/LoginForm'
 import SigninForm from '../components/SigninForm'
 
 const Home = () => {
+
+  const [randomFact, setRandomFact] = useState()
+
+  async function randomFactGenerator() {
+    const res = await fetch("https://catfact.ninja/fact")
+    const fact = await res.json()
+    setRandomFact(fact.fact)
+  }
+
   return (
     <>
       <h1>Home</h1>
       <h1>IMAGEN CON BOTÓN</h1>
+      <button onClick={ randomFactGenerator }>Random Fact</button>
+      <p>{ randomFact }</p>
       <LoginForm />
       <SigninForm />
     </>
